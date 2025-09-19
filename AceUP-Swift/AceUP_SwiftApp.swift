2688e569f83e32af0c1dfddc8875744e398a90a0
Ángel Farfán Arcila <farfanarcilaangel@gmail.com>
2025-09-19 18:02:33 -0500

    TODO

---
+++ AceUP-Swift/AceUP_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AceUP_SwiftApp.swift
+//  AceUP-Swift
+//
+//  Created by Julian David  Parra Forero on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AceUP_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AceUP-SwiftTests/AceUP_SwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  AceUP_SwiftTests.swift
+//  AceUP-SwiftTests
+//
+//  Created by Julian David  Parra Forero on 19/09/25.
+//
+
+import Testing
+
+struct AceUP_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
